c7a1bec69afdf2c7261ec6f83808dc42166524e6
Nived <nivedsuresh5@gmail.com>
2025-10-19 18:57:59 +0530

    authentication completed

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -12,10 +12,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: [
-      // "http://localhost:5173",
-      process.env.FRONTEND_URL!,
-    ],
+   origin: [process.env.FRONTEND_URL!, "http://localhost:5173"],
     credentials: true,
   })
 );
